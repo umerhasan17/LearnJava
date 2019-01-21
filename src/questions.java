ccf7e49a822727675500c73d490ef26aa1a211fd
umerhasan17 <umerhasan17@gmail.com>
2019-01-21 21:03:48 +0000

    First question attempt

---
+++ src/questions.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class questions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+    /* 014e: Random Numbers */
+
+    public static void RandomNumbers(int n) {
+        boolean[] found = new boolean[n];
+        for (int i = 0; i < n; i ++) {
+            found[i] = false;
+        }
+        Random generator = new Random();
+        int numbersFouund = 0;
+        int numbersGenerated = 0;
+        while (numbersFouund < n) {
+
+        }
+
+    }
+
+}
